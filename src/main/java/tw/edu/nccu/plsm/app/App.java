d5fe84805a265eec85a59101e541d59c165b74a3
atkinschang@plsm.cs.nccu.edu.tw <atkins@ck.plsm.cs.nccu.edu.tw>
2014-05-13 00:16:51 +0800

    init

---
+++ src/main/java/tw/edu/nccu/plsm/app/App.java
@@ -0,0 +1,13 @@
+package tw.edu.nccu.plsm.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
